3cd89b471b8650b307e565d814289cc6b666963f
Simon Sun <mengyuesun@gmail.com>
2020-11-22 12:50:40 -0600

    build results list

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const Search = () => {
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState('programming');
+  const [results, setResults] = useState([]);
+  console.log(results);
   useEffect(() => {
     const search = async () => {
-      await axios.get('https://en.wikipedia.org/w/api.php', {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
         params: {
           action: 'query',
           list: 'search',
@@ -14,20 +16,35 @@ const Search = () => {
           srsearch: term
         }
       });
+      setResults(data.query.search);
     };
     search();
   }, [term]);
-  return (
-    <div className="ui form">
-      <div className="fild">
-        <label>Enter search term</label>
-        <input
-          value={term}
-          type="text"
-          placeholder="Search..."
-          onChange={(e) => setTerm(e.target.value)}
-        />
+
+  const resultsList = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <div className="description">{result.snippet}</div>
+        </div>
       </div>
+    );
+  });
+  return (
+    <div className="ui container">
+      <div className="ui form">
+        <div className="fild">
+          <label>Enter search term</label>
+          <input
+            value={term}
+            type="text"
+            placeholder="Search..."
+            onChange={(e) => setTerm(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className="ui celled list">{resultsList}</div>
     </div>
   );
 };
