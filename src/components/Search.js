71e08abfcc82aae7eb952581b8ada2d5ac43f4cb
Simon Sun <mengyuesun@gmail.com>
2020-11-22 14:05:55 -0600

    add wikipedia link page

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 const Search = () => {
   const [term, setTerm] = useState('programming');
   const [results, setResults] = useState([]);
+  console.log(results);
   useEffect(() => {
     const search = async () => {
       const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
@@ -23,6 +24,14 @@ const Search = () => {
   const resultsList = results.map((result) => {
     return (
       <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+            className="ui button"
+          >
+            Go
+          </a>
+        </div>
         <div className="content">
           <div className="header">{result.title}</div>
           <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
